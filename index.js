d53ef840670ce0f1cefd243c7403ad3ceac847eb
Chanel Donahoe <cdonahoe05@gmail.com>
2021-08-01 00:43:42 +1000

    done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Polygon {
+    constructor(arr){
+    this.arr = arr;
+    this.count = this.arr.length
+    }
+
+    get countSides(){
+        return this.arr.length;
+    }
+    get perimeter(){
+        return this.arr.reduce((a,b) => a + b, 0)
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+      if (this.count !== 3) {
+          return false;
+      } else {
+        let arr1 = this.arr[0]
+        let arr2 = this.arr[1]
+        let arr3 = this.arr[2]
+        return ((arr1 + arr2 > arr3) && (arr1 + arr3 > arr2) && (arr3+ arr2 > arr1))
+      }
+    }
+  }
+
+  class Square extends Polygon{
+    
+    get area(){
+        return this.arr[1] * this.arr[2];
+    }
+
+    get isValid(){
+        if (this.count !== 4){
+            return false;
+        } else {
+        let arr1 = this.arr[0]
+        let arr2 = this.arr[1]
+        let arr3 = this.arr[2]
+        let arr4 = this.arr[3]
+        return (arr1 === arr2 && arr1 === arr3 && arr1 == arr4)
+        }
+    }
+  }
